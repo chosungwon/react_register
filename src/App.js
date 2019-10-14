9343790dd5475e88770ba9257beca8a9a114472d
조성원 <calras@Calras-2.local>
2019-10-14 22:36:58 +0900

    example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+      username: "",
+      pwd: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value,
+    })
+      console.log(this.state)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+        username: this.state.username,
+        pwd: this.state.pwd
+    }
+
+    axios.post('http://172.30.1.35:3001/register', user)
+        .then(res =>{
+          console.log(res.data)
+        })
+  }
+  render(){
+    return(
+        <div>
+          <form onSubmit={this.handleSubmit}>
+              <label>
+                id:<input type={"text"} name={"username"} onChange={this.handleChange}/>
+              </label>
+              <label>
+                password:<input type={"password"} name={"pwd"} onChange={this.handleChange}/>
+              </label>
+            <button type={"submit"}>회원가입</button>
+          </form>
+        </div>
+    )
+  }
 }
 
 export default App;
